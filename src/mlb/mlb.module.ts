baaa1c6c81aa39abe82268911a1af00fbf02842e
Brandon Harris <bharris@dminc.com>
2019-06-21 23:43:37 -0500

    HttpClientModule

--- src/mlb/mlb.module.ts
+++ src/mlb/mlb.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MlbComponent } from './mlb.component';
 import { MlbService } from './mlb.service';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   declarations: [MlbComponent],
   providers: [MlbService]
 })
--- src/wwc/wwc.module.ts
+++ src/wwc/wwc.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { WwcComponent } from './wwc.component';
 import { WwcService } from './wwc.service';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   declarations: [WwcComponent],
   providers: [WwcService]
 })
