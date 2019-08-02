9cdc6f6cf95dd58634325259b9a7379bb6093acb
Brandon Harris <brandonkharris@gmail.com>
2019-08-02 11:20:54 -0500

    Update services to use environments AB#12

--- src/mlb/mlb.service.ts
+++ src/mlb/mlb.service.ts
@@ -2,22 +2,23 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, timer } from 'rxjs';
 import { flatMap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { MlbBoxScore, MlbScore } from './mlb.model';
 
 @Injectable()
 export class MlbService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getScores(date: Date): Observable<MlbScore[]> {
     const yyyymmdd = this.yyyymmdd(date);
-    return this.http.get<MlbScore[]>(`http://localhost:3000/scores/mlb?date=${yyyymmdd}`);
+    return this.http.get<MlbScore[]>(`${environment.apiUrl}/scores/mlb?date=${yyyymmdd}`);
   }
 
   getBoxScore(id: string): Observable<MlbBoxScore> {
     return timer(0, 30000).pipe(
       flatMap(res => {
         return this.http.get<MlbBoxScore>(
-          `http://localhost:3000/scores/mlb/${id}`
+          `${environment.apiUrl}/scores/mlb/${id}`
         );
       })
     );
--- src/wwc/wwc.service.ts
+++ src/wwc/wwc.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { WwcBoxScore, WwcScore } from './wwc.model';
 
 @Injectable()
@@ -9,11 +10,11 @@ export class WwcService {
 
   getScores(date: Date): Observable<WwcScore[]> {
     const yyyymmdd = this.yyyymmdd(date);
-    return this.http.get<WwcScore[]>(`http://localhost:3000/scores/wwc?date=${yyyymmdd}`);
+    return this.http.get<WwcScore[]>(`${environment.apiUrl}/scores/soccer/fifa.wwc?date=${yyyymmdd}`);
   }
 
   getBoxScore(id: string): Observable<WwcBoxScore> {
-    return this.http.get<WwcBoxScore>(`http://localhost:3000/scores/wwc/${id}`);
+    return this.http.get<WwcBoxScore>(`${environment.apiUrl}/scores/soccer/fifa.wwc/${id}`);
   }
 
   private yyyymmdd(dateIn) {
