79ed6c2a40ed574d1efbb7cbf5020c088d49c651
Brandon Harris <brandonkharris@gmail.com>
2019-08-03 09:22:02 -0500

    Add loading spinner component AB#74

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BoxScoreComponent } from './box-score/box-score.component';
 import { DatePickerComponent } from './date-picker/date-picker.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 import { MaterialModule } from './material.module';
 import { ScoreCardComponent } from './score-card/score-card.component';
 import { TeamScoreComponent } from './team-score/team-score.component';
 
 @NgModule({
   imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
-  declarations: [TeamScoreComponent, ScoreCardComponent, BoxScoreComponent, DatePickerComponent],
-  exports: [TeamScoreComponent, ScoreCardComponent, BoxScoreComponent, DatePickerComponent],
+  declarations: [TeamScoreComponent, ScoreCardComponent, BoxScoreComponent, DatePickerComponent, LoadingSpinnerComponent],
+  exports: [TeamScoreComponent, ScoreCardComponent, BoxScoreComponent, DatePickerComponent, LoadingSpinnerComponent],
   providers: []
 })
 export class SharedModule {}
